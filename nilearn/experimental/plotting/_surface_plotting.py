e9dfaaf1cc36fb543b29e282a98fa7794ecc7bd3
Remi Gau <remi_gau@hotmail.com>
2024-02-09 11:35:58 +0100

    update doc strings

--- nilearn/experimental/plotting/_surface_plotting.py
+++ nilearn/experimental/plotting/_surface_plotting.py
@@ -1,116 +1,188 @@
+"""Experimental Surface plotting."""
+
+from __future__ import annotations
+
+import numpy
+
 from nilearn import plotting as old_plotting
-from nilearn.experimental.surface import PolyMesh, SurfaceImage
+from nilearn._utils import fill_doc
+from nilearn.experimental.surface import Mesh, PolyMesh, SurfaceImage
 
 
+@fill_doc
 def plot_surf(
-    img: SurfaceImage,
-    hemi: str | None = None,
-    surf_mesh: PolyMesh | None = None,
+    surf_map: SurfaceImage | str | numpy.array | None = None,
+    hemi: str = "left",
+    surf_mesh: str
+    | list[numpy.array, numpy.array]
+    | Mesh
+    | PolyMesh
+    | None = None,
     **kwargs,
 ):
     """Plot a SurfaceImage.
 
-    TODO: docstring.
+    Parameters
+    ----------
+    surf_map : SurfaceImage or :obj:`str` or numpy.ndarray, optional
+
+    surf_mesh : :obj:`str` or :obj:`list` of two numpy.ndarray \
+                or Mesh or PolyMesh, optional
+
+    %(hemi)s
     """
-    # TODO deal with surf_mesh, surf_map, hemi in kwargs
+    if not isinstance(surf_map, SurfaceImage):
+        return old_plotting.plot_surf(
+            surf_mesh=surf_mesh,
+            surf_map=surf_map,
+            hemi=hemi,
+            **kwargs,
+        )
+
     if surf_mesh is None:
-        surf_mesh = img.mesh
+        surf_mesh = surf_map.mesh
 
-    if hemi is None:
-        hemi = "left"
+    _check_hemi_present(surf_mesh, surf_map, hemi)
 
-    _check_hemi_present(surf_mesh, img, hemi)
-
-    fig = old_plotting.plot_surf(
+    return old_plotting.plot_surf(
         surf_mesh=surf_mesh[hemi],
-        surf_map=img.data[hemi],
+        surf_map=surf_map.data[hemi],
         hemi=hemi,
         **kwargs,
     )
-    return fig
 
 
+@fill_doc
 def plot_surf_stat_map(
-    img: SurfaceImage,
-    hemi: str | None = None,
-    surf_mesh: PolyMesh | None = None,
+    stat_map: SurfaceImage | str | numpy.array | None = None,
+    hemi: str = "left",
+    surf_mesh: str
+    | list[numpy.array, numpy.array]
+    | Mesh
+    | PolyMesh
+    | None = None,
     **kwargs,
 ):
     """Plot a stats map on a SurfaceImage.
 
-    TODO: docstring.
+    Parameters
+    ----------
+    stat_map: SurfaceImage or :obj:`str` or numpy.ndarray, optional
+
+    surf_mesh : :obj:`str` or :obj:`list` of two numpy.ndarray \
+                or Mesh or PolyMesh, optional
+
+    %(hemi)s
     """
-    # TODO deal with stat_map in kwargs
+    if not isinstance(stat_map, SurfaceImage):
+        return old_plotting.plot_surf_stat_map(
+            surf_mesh=surf_mesh,
+            surf_map=stat_map,
+            hemi=hemi,
+            **kwargs,
+        )
+
     if surf_mesh is None:
-        surf_mesh = img.mesh
+        surf_mesh = stat_map.mesh
 
-    if hemi is None:
-        hemi = "left"
-
-    _check_hemi_present(surf_mesh, img, hemi)
+    _check_hemi_present(surf_mesh, stat_map, hemi)
 
     fig = old_plotting.plot_surf_stat_map(
         surf_mesh=surf_mesh[hemi],
-        stat_map=img.data[hemi],
+        stat_map=stat_map.data[hemi],
         hemi=hemi,
         **kwargs,
     )
     return fig
 
 
+@fill_doc
 def plot_surf_contours(
-    img: SurfaceImage,
-    hemi: str | None = None,
-    surf_mesh: PolyMesh | None = None,
+    roi_map: SurfaceImage | str | numpy.array | list[numpy.ndarray],
+    hemi: str = "left",
+    surf_mesh: str
+    | list[numpy.array, numpy.array]
+    | Mesh
+    | PolyMesh
+    | None = None,
     **kwargs,
 ):
     """Plot a contours on a SurfaceImage.
 
-    TODO: docstring.
+    Parameters
+    ----------
+    roi_map : SurfaceImage or str or numpy.ndarray or list of numpy.ndarray
+
+    surf_mesh : :obj:`str` or :obj:`list` of two numpy.ndarray \
+                or Mesh or PolyMesh, optional
+
+    %(hemi)s
     """
-    # TODO deal with roi_map in kwargs
+    if not isinstance(roi_map, SurfaceImage):
+        return old_plotting.plot_surf_contours(
+            surf_mesh=surf_mesh,
+            roi_map=roi_map,
+            hemi=hemi,
+            **kwargs,
+        )
+
     if surf_mesh is None:
-        surf_mesh = img.mesh
+        surf_mesh = roi_map.mesh
 
-    if hemi is None:
-        hemi = "left"
-
-    _check_hemi_present(surf_mesh, img, hemi)
+    _check_hemi_present(surf_mesh, roi_map, hemi)
 
     fig = old_plotting.plot_surf_contours(
         surf_mesh=surf_mesh[hemi],
-        roi_map=img.data[hemi],
+        roi_map=roi_map.data[hemi],
         hemi=hemi,
         **kwargs,
     )
     return fig
 
 
+@fill_doc
 def view_surf(
-    img: SurfaceImage,
-    hemi: str | None = None,
-    surf_mesh: PolyMesh | None = None,
+    surf_map: SurfaceImage | str | numpy.array | None = None,
+    hemi: str = "left",
+    surf_mesh: str
+    | list[numpy.array, numpy.array]
+    | Mesh
+    | PolyMesh
+    | None = None,
     **kwargs,
 ):
     """View SurfaceImage in browser.
 
-    TODO: docstring.
+    Parameters
+    ----------
+    surf_map : SurfaceImage or str or numpy.ndarray, optional
+
+    surf_mesh : :obj:`str` or :obj:`list` of two numpy.ndarray \
+                or Mesh or PolyMesh, optional
+
+    %(hemi)s
     """
+    if not isinstance(surf_map, SurfaceImage):
+        return old_plotting.view_surf(
+            surf_mesh=surf_mesh,
+            roi_map=surf_map,
+            hemi=hemi,
+            **kwargs,
+        )
+
     if surf_mesh is None:
-        surf_mesh = img.mesh
+        surf_mesh = surf_map.mesh
 
-    if hemi is None:
-        hemi = "left"
-
-    _check_hemi_present(surf_mesh, img, hemi)
+    _check_hemi_present(surf_mesh, surf_map, hemi)
 
     fig = old_plotting.view_surf(
-        surf_mesh=surf_mesh[hemi], surf_map=img.data[hemi], **kwargs
+        surf_mesh=surf_mesh[hemi], surf_map=surf_map.data[hemi], **kwargs
     )
     return fig
 
 
 def _check_hemi_present(mesh: PolyMesh, img: SurfaceImage, hemi: str):
+    """Check that a given hemisphere exists both in data and mesh."""
     if hemi not in mesh or hemi not in img.data:
         raise ValueError(
             f"{hemi} must be present in mesh and SurfaceImage data"
--- nilearn/experimental/plotting/tests/test_surface_plotting.py
+++ nilearn/experimental/plotting/tests/test_surface_plotting.py
@@ -3,9 +3,9 @@ from nilearn.experimental.plotting import plot_surf, plot_surf_stat_map
 
 def test_plot_surf(mini_surface_img):
     """Smoke test."""
-    plot_surf(img=mini_surface_img)
+    plot_surf(surf_map=mini_surface_img)
 
 
 def test_plot_surf_stat_map(mini_surface_img):
     """Smoke test."""
-    plot_surf_stat_map(img=mini_surface_img)
+    plot_surf_stat_map(stat_map=mini_surface_img)
