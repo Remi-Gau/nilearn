f31cd698b6f98dfa1e3c0b7f1d3deade1d2fb880
Remi Gau <remi_gau@hotmail.com>
2024-06-11 13:56:21 +0200

    refactor

--- tmp.py
+++ tmp.py
@@ -2,11 +2,13 @@
 
 from pathlib import Path
 
+from nilearn.experimental.reporting.glm_reporter import (
+    _make_surface_glm_report,
+)
 from nilearn.glm.first_level import FirstLevelModel
-from nilearn.reporting.glm_reporter import _make_surface_glm_report
 
 model = FirstLevelModel()
 
 report = _make_surface_glm_report(model)
 
-report.save_as_html(Path() / "tmp.html")
+report.save_as_html(Path() / "results" / "tmp.html")
