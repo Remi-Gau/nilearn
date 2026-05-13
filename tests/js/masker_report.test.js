6575ac09ab80948a9f4e90bea369eff39bd3896f
Remi Gau <remi_gau@hotmail.com>
2026-05-13 20:12:01 +0200

    reuse generated report for testing

---
+++ tests/js/masker_report.test.js
@@ -0,0 +1,11 @@
+const template = require('./template.js')
+
+const maskers = ['NiftiMasker', 'NiftiLabelsMasker', 'NiftiMapsMasker', 'SurfaceMasker', 'SurfaceLabelsMasker', 'SurfaceMapsMasker']
+
+for (let i = 0; i < maskers.length; i++) {
+  template.fullTest(maskers[i] + '_fitted.html',
+  // height was adapted to crop footer from png
+  // to avoid the change in nilearn version from appearing
+    { x: 0, y: 0, width: 800, height: 550 }
+  )
+}
